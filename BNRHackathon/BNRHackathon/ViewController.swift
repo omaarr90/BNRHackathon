4bef103412d1c8775276efca5662276bca271f76
Mohamad Kaakati <mt.wicked@gmail.com>
2015-12-03 16:14:43 -0500

    360 Rotation Animation for a redBox

--- BNRHackathon/BNRHackathon/ViewController.swift
+++ BNRHackathon/BNRHackathon/ViewController.swift
@@ -8,9 +8,41 @@
 
 import UIKit
 
+
+
+extension UIView {
+    
+    func rotate360Degrees(duration: CFTimeInterval = 120.0, completionDelegate: AnyObject? = nil) {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.fromValue = 0.0
+        rotateAnimation.toValue = CGFloat(M_PI * 500.0)
+        rotateAnimation.duration = duration
+        
+        if let delegate: AnyObject = completionDelegate {
+            rotateAnimation.delegate = delegate
+        }
+        self.layer.addAnimation(rotateAnimation, forKey: nil)
+    }
+    
+    func positionMove(duration: CFTimeInterval = 10.0, completionDelegate: AnyObject? = nil) {
+        let positionAnimation = CABasicAnimation(keyPath: "position")
+        positionAnimation.toValue = NSValue(CGPoint: CGPointMake(layer.position.x, 0.0))
+        positionAnimation.duration = duration
+        
+        if let delegate: AnyObject = completionDelegate {
+            positionAnimation.delegate = delegate
+        }
+        self.layer.addAnimation(positionAnimation, forKey: nil)
+    }
+
+    
+}
+
 class ViewController: UIViewController {
 
     var omarView: OmarView!
+    
+    @IBOutlet weak var redView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -84,6 +116,10 @@ class ViewController: UIViewController {
 //        smartPin.view = self.view
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        redView.rotate360Degrees()
+        redView.positionMove()
+        
 
     func handleSingleTap(sender: UIGestureRecognizer)
     {
